50ce4b2fdfd183c1f495fe0ec172c3044a54d599
anthoxdu13 <79569971+anthoxdu13@users.noreply.github.com>
2024-08-17 22:39:31 +0200

    Add Modals css & js

---
+++ assets/Script/modal.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    document.getElementById('reviewModal').style.display = 'block';
+}, 30000);
+
+// Close modal function
+function closeModal() {
+    document.getElementById('reviewModal').style.display = 'none';
+}
